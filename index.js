3f5ea32f8162cbf1a053b67dd6db87b0a5add724
Michel Sebaaly <michelsebaaly4@gmail.com>
2024-12-09 22:17:59 +0200

    adding an api that get employees from an excel file

---
+++ index.js
@@ -0,0 +1,19 @@
+var cors = require("cors");
+var XLSX = require("xlsx");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+app.use(cors());
+
+app.get("/employees", function (req, res) {
+  const fileBuffer = fs.readFileSync("./info/data.xlsx");
+  const workbook = XLSX.read(fileBuffer, { type: "buffer" });
+
+  const sheetName = "employees";
+  const sheet = workbook.Sheets[sheetName];
+  const data = XLSX.utils.sheet_to_json(sheet);
+  res.send(data);
+});
+
+app.listen(3000);
